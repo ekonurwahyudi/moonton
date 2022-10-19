17804b39a6a57833bf39169561554639e5269834
ekonurwahyudi <generasic@gmail.com>
2022-10-19 12:27:27 +0700

    feat: init Breeze

---
+++ dist/app.js
@@ -0,0 +1,28 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./dist/app.js ***!
+  \*********************/
+/******/(function () {
+  // webpackBootstrap
+  var __webpack_exports__ = {};
+  /*!*********************!*\
+    !*** ./dist/app.js ***!
+    \*********************/
+  /******/
+  (function () {
+    // webpackBootstrap
+    /******/
+    "use strict";
+
+    /******/
+    /******/
+    /******/
+  })();
+
+  alert('hello world');
+  ;
+  /******/
+})();
+/******/ })()
+;
